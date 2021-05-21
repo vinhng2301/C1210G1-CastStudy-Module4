55c0e7c2ef343bfe775e915a74f5be60533badfe
vinhng2301 <ducvinhnguyen0201@gmail.com>
2021-05-21 10:09:39 +0700

    fixed create

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -16,16 +19,43 @@ public class AdminController {
 
     @GetMapping("/create-product")
     public ModelAndView showCreateForm() {
-        ModelAndView modelAndView = new ModelAndView("/create-product");
+        ModelAndView modelAndView = new ModelAndView("/product/create");
         modelAndView.addObject("product", new Product());
         return modelAndView;
     }
     @PostMapping("/create-product")
-    public ModelAndView saveCustomer(@ModelAttribute("product") Product product) {
+    public ModelAndView saveProduct(@ModelAttribute("product") Product product) {
         productService.save(product);
-        ModelAndView modelAndView = new ModelAndView("/create-product");
+        ModelAndView modelAndView = new ModelAndView("/product/create");
         modelAndView.addObject("product", new Product());
-        modelAndView.addObject("message", "New product created successfully");
+        modelAndView.addObject("message", "New product created");
+        return modelAndView;
+    }
+
+    @GetMapping("/products")
+    public ModelAndView listProducts() {
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        modelAndView.addObject("products", productService.findAll());
+        return modelAndView;
+    }
+    @GetMapping("/edit-product/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Optional<Product> product = Optional.ofNullable(productService.findById(id));
+        if (product.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/product/edit");
+            modelAndView.addObject("product", product.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/error.404");
+        }
+    }
+
+    @PostMapping("/edit-product")
+    public ModelAndView updateProduct(@ModelAttribute("product") Product product) {
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/product/edit");
+        modelAndView.addObject("product", product);
+        modelAndView.addObject("message", "Product updated");
         return modelAndView;
     }
 }
